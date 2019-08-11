be6d0ec769b3c3eb6595989269c3039f69cafe47
nerya <neryasheetirt@gmail.com>
2019-08-11 20:15:24 +0300

    Initial commit

---
+++ app/src/main/java/com/nerya/uber/MainActivity.java
@@ -0,0 +1,132 @@
+package com.nerya.uber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class MainActivity extends AppCompatActivity {
+private Button signup, onetime;
+private EditText password, username, pOd;
+private RadioButton passenger, driver;
+    enum State {
+        SIGNUP, LOGIN
+    }
+    private State state;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        signup = findViewById(R.id.signup);
+        onetime = findViewById(R.id.onetime);
+
+        password = findViewById(R.id.password);
+        pOd = findViewById(R.id.PorD);
+        username = findViewById(R.id.username);
+
+        passenger = findViewById(R.id.passenger);
+        driver = findViewById(R.id.driver);
+
+        if(ParseUser.getCurrentUser() != null) {
+            ParseUser.logOut();
+        }
+state = State.SIGNUP;
+signup.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        if(state == State.SIGNUP){
+            if(driver.isChecked() == false && passenger.isChecked() == false){
+                Toast.makeText(MainActivity.this, "driver or passenger?", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            ParseUser appUser = new ParseUser();
+            appUser.setUsername(username.getText().toString());
+            appUser.setPassword(password.getText().toString());
+            if(driver.isChecked() == true){
+                appUser.put("as", "driver");
+            }else {
+                appUser.put("as", "passenger");
+            }
+            appUser.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e == null){
+                        Toast.makeText(MainActivity.this, "Signed Up successfully!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }else {
+            ParseUser.logInInBackground(username.getText().toString(), password.getText().toString(), new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+                    if(user != null && e == null){
+                        Toast.makeText(MainActivity.this, "logged in succesfully", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+});
+onetime.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        if(pOd.getText().toString().equals("Driver") || pOd.getText().toString().equals("Passenger")){
+            if(ParseUser.getCurrentUser() == null){
+                ParseAnonymousUtils.logIn(new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if(user != null && e == null){
+                            Toast.makeText(MainActivity.this, "you logged in anonymously", Toast.LENGTH_SHORT).show();
+                            user.put("as", pOd.getText().toString());
+                            user.saveInBackground();
+                        }else{
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }else {
+            Toast.makeText(MainActivity.this, "are you a driver or a passenger?", Toast.LENGTH_SHORT).show();
+        }
+    }
+});
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+                if (state == State.SIGNUP) {
+                    state = State.LOGIN;
+                    item.setTitle("Sign Up");
+                    signup.setText("Log In");
+                } else if (state == State.LOGIN) {
+
+                    state = State.SIGNUP;
+                    item.setTitle("Log In");
+                    signup.setText("Sign Up");
+                }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
